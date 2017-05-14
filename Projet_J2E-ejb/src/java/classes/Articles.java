7a311647ebb057f33333c601fe801ac381589243
Camille <Camille>
2017-05-14 13:49:50 +0200

    Implémentation code Article : Création, Suppression, Modif etc.

--- Projet_J2E-ejb/src/java/classes/Articles.java
+++ Projet_J2E-ejb/src/java/classes/Articles.java
@@ -5,6 +5,58 @@
  */
 package classes;
 
+import java.util.ArrayList;
+
 public class Articles {
+    public ArrayList<Articles> articles;
+    private int ref;
+    private String type;
+    private double prixUnitaire;
+    private int stock;
+    private STATUT statutA = STATUT.DISPONIBLE;
+
+    private Articles() {
+        this.ref = 0;
+        this.type = null;
+        this.prixUnitaire = 0;
+        this.stock = 0;
+    }
+    public enum STATUT {
+        DISPONIBLE, INDISPONIBLE;
+    }
+
+    public Articles(int ref, String type, double prixUnitaire, int stock) {
+        this.ref = ref;
+        this.type = type;
+        this.prixUnitaire = prixUnitaire;
+        this.stock = stock;
+    }
+
+    public void addArticle(){
+        Articles newA = new Articles();
+        articles.add(newA);
+    }
+ 
+    public void deleteArticles(int ref){
+        articles.remove(ref);
+    }
+    
+    public void majproduct(int ref, int stock){
+        Articles article = articles.get(ref);
+        article.stock = stock;
+        articles.set(ref, article);
+    }
+    
+    public void statutBecomeIndispo(int ref){
+        Articles article = articles.get(ref);
+        if (article.statutA == STATUT.DISPONIBLE){
+            article.statutA = STATUT.INDISPONIBLE;
+        }
+        else{
+            article.statutA = STATUT.DISPONIBLE;
+        }
+        articles.set(ref, article);
+    }
+    
     
 }
